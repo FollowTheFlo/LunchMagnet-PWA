90bf1f3777cc874f1b5bf0810cbee7fd92c2c04b
FollowTheFlo <florent.letendre@hotmail.fr>
2022-01-10 00:44:17 -0500

    fix: error when incoming orders list is null

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -14,7 +14,7 @@ import { User } from "./models/user.model";
 
 export default class Utils {
   static deepCopyOrdersList(l: Order[]): Order[] {
-    return l.map((o) => this.deepCopyOrder(o));
+    return l?.map((o) => this.deepCopyOrder(o)) ?? null;
   }
   static deepCopyOrder(o: Order): Order {
     return {
